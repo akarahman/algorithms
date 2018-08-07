a04d300be0080261b757e8c2c171ff9bf7c004f7
akarahman <kuadilah@umich.edu>
2018-08-06 22:05:23 -0400

    RMEs

--- binary-search/binary_search.h
+++ binary-search/binary_search.h
@@ -3,12 +3,28 @@
 #include <functional>
 #include <cassert>
 
+/*
+ * Requires: midpoint index is relative to the original size of the array (not subarray)
+ *           v is the entire array provided at the start of the search
+ * Effects: prints out the subarray and a pointer to the midpoint
+ */ 
 template <class T>
 void print_vector(const std::vector<T> &v, int midpoint, int start, int end);
+/*
+ * Requires: midpoint index is relative to the original size of the array (not subarray)
+ *           v is the entire array provided at the start of the search
+ *           comp takes two arguments and returns true if lhs < rhs
+ * Effects: recursively searches v to find key and returns the index of the key if found, else -1
+ */ 
 template <class T, class Compare>
 int binary_search_helper(T key, const std::vector<T> &v, int start, int end, Compare comp);
+/*
+ * Requires: comp takes two arguments and returns true if lhs < rhs
+ * Effects: returns the index of the key if found, else -1
+ */ 
 template <class T, class Compare = std::less<T> >
 int binary_search(T key, const std::vector<T> &v, Compare comp = Compare());
+
 
 template <class T, class Compare>
 int binary_search_helper(T key, const std::vector<T> &v, int start, int end, Compare comp)
@@ -50,6 +66,7 @@ void print_vector(const std::vector<T> &v, int midpoint, int start, int end)
         std::cout << v[i] << " ";
     }
     std::cout << "]\n";
+    // TODO: fix this
     // int pos = 2 + (midpoint-start)*2;
     // for (int i = 0; i < pos; ++i)
     // {
