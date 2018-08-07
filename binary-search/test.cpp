eb5aece47e494e4ba49ba12454b814479cd54986
akarahman <kuadilah@umich.edu>
2018-08-06 21:42:22 -0400

    more tests

--- binary-search/test.cpp
+++ binary-search/test.cpp
@@ -12,6 +12,8 @@ int main(int argc, char const *argv[])
     test_empty();
     test_single_elem_exist();
     test_single_elem_nonexist();
+    test_sorted_array_exist();
+    test_sorted_array_nonexist();
 }
 
 void test_empty()
@@ -41,4 +43,35 @@ void test_single_elem_nonexist()
     int index = binary_search<int>(2, v);
     assert(index == -1);
     std::cout << "\n";
+}
+
+void test_sorted_array_exist()
+{
+    std::cout << "Testing binary search on a sorted vector...\n";
+
+    std::vector<int> v;
+    for (int i = 0; i < 12; ++i)
+    {
+        v.push_back(i);
+    }
+    int index;
+    for (int i = 0; i < 12; ++i)
+    {
+        index = binary_search<int>(i, v);
+        assert(v[index] == i);
+        std::cout << v[index] << " found at index " << index << "\n\n";
+    }
+}
+
+void test_sorted_array_nonexist()
+{
+    std::cout << "Testing binary search on a sorted vector...\n";
+
+    std::vector<int> v;
+    for (int i = 0; i < 12; ++i)
+    {
+        v.push_back(i*i);
+    }
+    assert(binary_search<int>(3, v) == -1);
+    assert(binary_search<int>(101, v) == -1);
 }
