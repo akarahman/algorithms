90e68538b8624c963c1ae66ac983882b8d4b03c9
akarahman <kuadilah@umich.edu>
2018-08-23 16:01:22 -0400

    comments

--- search/paint-fill/paint_fill.c
+++ search/paint-fill/paint_fill.c
@@ -2,20 +2,25 @@
 
 void fill_helper(int** canvas, int n, int m, int x, int y, int color, int ori, int **done)
 {
+    // update color and mark as done to avoid revisiting
     if (canvas[x][y] != color) canvas[x][y] = color;
     done[x][y] = 1;
+    // check cell above
     if (x-1 >= 0 && !done[x-1][y] && (canvas[x-1][y] == ori || canvas[x-1][y] == color)) 
     {
         fill_helper(canvas, n, m, x-1, y, color, ori, done);
     }
+    // check cell below
     if (x+1 < n && !done[x+1][y] && (canvas[x+1][y] == ori || canvas[x+1][y] == color)) 
     {
         fill_helper(canvas, n, m, x+1, y, color, ori, done);
     }
+    // check left cell
     if (y-1 >= 0 && !done[x][y-1] && (canvas[x][y-1] == ori || canvas[x][y-1] == color)) 
     {
         fill_helper(canvas, n, m, x, y-1, color, ori, done);
     }
+    // check right cell
     if (y+1 < m && !done[x][y+1] && (canvas[x][y+1] == ori || canvas[x][y+1] == color)) 
     {
         fill_helper(canvas, n, m, x, y+1, color, ori, done);
--- search/paint-fill/paint_fill.h
+++ search/paint-fill/paint_fill.h
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * Modifies: canvas, done
+ * Effects: fills all cells == ori that are reachable from point (x, y) with color 
+ */
 void fill_helper(int** canvas, int n, int m, int x, int y, int color, int ori, int **done);
+
+/*
+ * Modifies: canvas
+ * Effects: fills all cells == ori that are reachable from point (x, y) with color 
+ */
 void fill(int** canvas, int n, int m, int x, int y, int color);
+
+/*
+ * Effects: prints all values in canvas
+ */
 void print_canvas(int** canvas, int n, int m);
