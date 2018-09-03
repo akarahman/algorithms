9a6cc3d259f68098b24779f7268bdcca3239464f
akarahman <kuadilah@umich.edu>
2018-09-03 02:01:17 -0400

    code cleanup and optimization

--- combinatorial/bool-eval/bool_eval.cpp
+++ combinatorial/bool-eval/bool_eval.cpp
@@ -1,6 +1,6 @@
 #include "bool_eval.h"
 
-int num_paren(std::string expr, bool res)
+int num_paren_helper(std::string expr, bool res, std::unordered_map<std::pair<std::string, bool>, int, pair_hash> &memo)
 {
     if (expr.size() == 0) return 0;
     if (expr.size() == 1) 
@@ -11,58 +11,112 @@ int num_paren(std::string expr, bool res)
         else throw std::exception();
         return (val == res) ? 1 : 0;
     }
+    std::pair<std::string, bool> p = std::make_pair(expr, res);
+    if (memo.find(p) != memo.end()) return memo[p];
 
     int count = 0;
     for (int i = 1; i < expr.size(); i += 2)
     {
-        if (res)
+        int right_true = num_paren_helper(expr.substr(0, i), true, memo);
+        int right_false = num_paren_helper(expr.substr(0, i), false, memo);
+        int left_true = num_paren_helper(expr.substr(i+1), true, memo);
+        int left_false = num_paren_helper(expr.substr(i+1), false, memo);
+
+        int total_parens = (right_true + right_false) * (left_true + left_false);
+        int total_true = 0;
+
+        if (expr[i] == '&')
         {
-            if (expr[i] == '&')
-            {
-                count += num_paren(expr.substr(0, i), true) *
-                         num_paren(expr.substr(i+1), true);
-            }
-            if (expr[i] == '|')
-            {
-                count += num_paren(expr.substr(0, i), true) *
-                         num_paren(expr.substr(i+1), true);
-                count += num_paren(expr.substr(0, i), false) *
-                         num_paren(expr.substr(i+1), true);
-                count += num_paren(expr.substr(0, i), true) *
-                         num_paren(expr.substr(i+1), false);
-            }
-            if (expr[i] == '^')
-            {
-                count += num_paren(expr.substr(0, i), true) *
-                         num_paren(expr.substr(i+1), false);
-                count += num_paren(expr.substr(0, i), false) *
-                         num_paren(expr.substr(i+1), true);
-            } 
+            total_true += right_true * left_true;
         }
-        else
+        if (expr[i] == '|')
         {
-            if (expr[i] == '&')
-            {
-                count += num_paren(expr.substr(0, i), true) *
-                         num_paren(expr.substr(i+1), false);
-                count += num_paren(expr.substr(0, i), false) *
-                         num_paren(expr.substr(i+1), true);
-                count += num_paren(expr.substr(0, i), false) *
-                         num_paren(expr.substr(i+1), false);
-            }
-            if (expr[i] == '|')
-            {
-                count += num_paren(expr.substr(0, i), false) *
-                         num_paren(expr.substr(i+1), false);
-            }
-            if (expr[i] == '^')
-            {
-                count += num_paren(expr.substr(0, i), true) *
-                         num_paren(expr.substr(i+1), true);
-                count += num_paren(expr.substr(0, i), false) *
-                         num_paren(expr.substr(i+1), false);
-            }
+            total_true += right_true * left_true + 
+                          right_true * left_false + 
+                          right_false * left_true;
         }
+        if (expr[i] == '^')
+        {
+            total_true += right_true * left_false + right_false * left_true;
+        } 
+
+        count += res ? total_true : total_parens - total_true;
     }
+
+    memo[p] = count;
     return count;
 }
+
+int num_paren(const std::string &expr, bool res)
+{
+    std::unordered_map<std::pair<std::string, bool>, int, pair_hash> memo;
+    return num_paren_helper(expr, res, memo);
+}
+
+
+// int num_paren(std::string expr, bool res)
+// {
+//     if (expr.size() == 0) return 0;
+//     if (expr.size() == 1) 
+//     {
+//         bool val;
+//         if (expr[0] == '0') val = false;
+//         else if (expr[0] == '1') val = true;
+//         else throw std::exception();
+//         return (val == res) ? 1 : 0;
+//     }
+
+//     int count = 0;
+//     for (int i = 1; i < expr.size(); i += 2)
+//     {
+//         if (res)
+//         {
+//             if (expr[i] == '&')
+//             {
+//                 count += num_paren(expr.substr(0, i), true) *
+//                          num_paren(expr.substr(i+1), true);
+//             }
+//             if (expr[i] == '|')
+//             {
+//                 count += num_paren(expr.substr(0, i), true) *
+//                          num_paren(expr.substr(i+1), true);
+//                 count += num_paren(expr.substr(0, i), false) *
+//                          num_paren(expr.substr(i+1), true);
+//                 count += num_paren(expr.substr(0, i), true) *
+//                          num_paren(expr.substr(i+1), false);
+//             }
+//             if (expr[i] == '^')
+//             {
+//                 count += num_paren(expr.substr(0, i), true) *
+//                          num_paren(expr.substr(i+1), false);
+//                 count += num_paren(expr.substr(0, i), false) *
+//                          num_paren(expr.substr(i+1), true);
+//             } 
+//         }
+//         else
+//         {
+//             if (expr[i] == '&')
+//             {
+//                 count += num_paren(expr.substr(0, i), true) *
+//                          num_paren(expr.substr(i+1), false);
+//                 count += num_paren(expr.substr(0, i), false) *
+//                          num_paren(expr.substr(i+1), true);
+//                 count += num_paren(expr.substr(0, i), false) *
+//                          num_paren(expr.substr(i+1), false);
+//             }
+//             if (expr[i] == '|')
+//             {
+//                 count += num_paren(expr.substr(0, i), false) *
+//                          num_paren(expr.substr(i+1), false);
+//             }
+//             if (expr[i] == '^')
+//             {
+//                 count += num_paren(expr.substr(0, i), true) *
+//                          num_paren(expr.substr(i+1), true);
+//                 count += num_paren(expr.substr(0, i), false) *
+//                          num_paren(expr.substr(i+1), false);
+//             }
+//         }
+//     }
+//     return count;
+// }
--- combinatorial/bool-eval/bool_eval.h
+++ combinatorial/bool-eval/bool_eval.h
@@ -1,5 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
+#include <functional>
+#include <string>
 
-int num_paren(std::string expr, bool res);
+struct pair_hash
+{
+    size_t operator() (const std::pair<std::string, bool> &p) const noexcept
+    {
+        std::string to_hash;
+        if (p.second) to_hash = p.first + 'a';
+        else to_hash = p.first + 'b';
+        size_t hash = std::hash<std::string>{}(to_hash);
+        return hash;
+    }
+};
+
+int num_paren_helper(std::string expr, bool res, std::unordered_map<std::pair<std::string, bool>, int, pair_hash> &memo);
+int num_paren(const std::string &expr, bool res);
 
