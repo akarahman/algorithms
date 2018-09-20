71b7d22b3e5320301cf163c415dc91ee16a4667e
Adilah K <kuadilah@umich.edu>
2018-09-19 20:51:20 -0400

    implementation for dlist

--- linked-lists/palindrome/palindrome.cpp
+++ linked-lists/palindrome/palindrome.cpp
@@ -4,19 +4,29 @@
 
 #include <iostream>
 #include <fstream>
+#include <cassert>
 
 struct node
 {
     int datum;
     node* next;
+    node* prev;
     node(node* n) : datum(n->datum), next(n->next) {}
     node(int d_in, node* n_in) : datum(d_in), next(n_in) {}
 };
 
 /*
+ * Requires: head points to a singly-linked list
  * Effects: returns true if the list is palindromic, false otherwise
  */
-bool palindrome(node* head);
+bool spalindrome(node* head);
+
+/*
+ * Requires: head points to a doubly-linked list
+ *           tail points to the last element of the list 
+ * Effects: returns true if the list is palindromic, false otherwise
+ */
+bool dpalindrome(node* head, node* tail);
 
 int main(int argc, char const *argv[])
 {
@@ -32,13 +42,16 @@ int main(int argc, char const *argv[])
         while (infile >> x)
         {
             n = new node(x, nullptr);
+            n->prev = prev;
             if (prev != nullptr) prev->next = n;
             prev = n;
             if (node_start == nullptr) node_start = n;
         }
         infile.close();
 
-        bool res = palindrome(node_start);
+        bool res = spalindrome(node_start);
+        bool other = dpalindrome(node_start, n);
+        assert(res == other);
 
         std::cout << argv[i] << " list ";
         if (res) std::cout << "is a palindrome.\n";
@@ -50,7 +63,7 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-bool palindrome(node* head)
+bool spalindrome(node* head)
 {
     if (!head || !(head->next)) return true;
     node* fast = head;
@@ -84,6 +97,20 @@ bool palindrome(node* head)
         node* del = second_half;
         second_half = second_half->next;
         delete del;
+    }
+    return true;
+}
+
+bool dpalindrome(node* head, node* tail)
+{
+    if (!head || head == tail) return true;
+    node* front = head;
+    node* back = tail;
+    while (front != back && front->prev != back)
+    {
+        if (front->datum != back->datum) return false;
+        front = front->next;
+        back = back->prev;
     }
     return true;
 }
